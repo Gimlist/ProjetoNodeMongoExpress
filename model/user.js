cf4691afb5e3c51f61d611d07836f0c657613f21
Guilherme Rodrigues de Andrade Silva <guilhermerod.a@gmail.com>
2019-05-30 10:42:50 -0300

    Update user.js

--- model/user.js
+++ model/user.js
@@ -25,7 +25,7 @@ UserSchema.pre('save', async function (next) {
     if (!user.isModified('password')) return next();
 
     //função para criptografar senha, com 10 processos de encriptação
-    user.passwordpassword = bcrypt.hash(user.password, 10);
+    user.password = bcrypt.hash(user.password, 10);
     //segue para o próximo
     return next();
 });
